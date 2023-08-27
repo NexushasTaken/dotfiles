224ea3802ef321201ed798baa2cb351fc7b1444a
NexushasTaken <nexushastaken@gmail.com>
2023-08-27 19:47:11 +0800

    Major improvements implementation for bash prompt(ps1) and tool

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 source $BASH_CONFIG_PATH/git-prompt.sh
+GIT_PS1_HIDE_IF_PWD_IGNORED='1'
+GIT_PS1_SHOWCOLORHINTS='1'
+GIT_PS1_STATESEPARATOR=''
 GIT_PS1_SHOWDIRTYSTATE='1'
 GIT_PS1_SHOWSTASHSTATE='1'
 GIT_PS1_SHOWUNTRACKEDFILES='1'
-GIT_PS1_HIDE_IF_PWD_IGNORED='1'
 GIT_PS1_SHOWUPSTREAM='auto'
-GIT_PS1_STATESEPARATOR=
-GIT_PS1_SHOWCOLORHINTS='1'
 GIT_PS1_COMPRESSSPARSESTATE='1'
 GIT_PS1_SHOWCONFLICTSTATE='1'
 GIT_PS1_DESCRIBE_STYLE='branch'
+PS1_DISABLE_GIT=''
+PS1_DIR_PARSE='1'
+PS1_DIR_SHORTEN=''
 
 function __ps1_to_ansi() {
   printf '\001\e[%sm\002' $1
@@ -30,31 +33,57 @@ function __ps1_ansi() {
   esac
 }
 
+function __print_variable() {
+  local out
+  out+="$(__ps1_ansi yellow)$1"
+  out+="$(__ps1_ansi magenta)($3)"
+  out+="$(__ps1_ansi white): "
+  out+="$(__ps1_ansi blue)$4"
+  out+="$(__ps1_ansi reset) = "
+  if [[ $4 = 'bool' ]]; then
+    out+="$(__ps1_ansi orange)"
+    if [[ $2 = '1' ]]; then
+      out+='true'
+    elif [[ -z $2 ]]; then
+      out+='false'
+    fi
+  else
+    out+="$(__ps1_ansi green)$2"
+  fi
+  echo "$out$(__ps1_ansi reset)"
+}
+
 function __ps1_help() {
-  print_variable() {
-    echo "$(__ps1_ansi yellow)$2$(__ps1_ansi magenta)($1)$(__ps1_ansi white): $(__ps1_ansi green)$3$(__ps1_ansi reset)"
-  }
-  print_variable 'sds' "GIT_PS1_SHOWDIRTYSTATE" $GIT_PS1_SHOWDIRTYSTATE
+  # $1 = Variable name
+  # $2 = Variable value
+  # $3 = Variable flag
+  # $4 = Variable type
+  __print_variable "GIT_PS1_HIDE_IF_PWD_IGNORED" "$GIT_PS1_HIDE_IF_PWD_IGNORED" 'hipi' 'bool'
+  __print_variable "GIT_PS1_SHOWCOLORHINTS" "$GIT_PS1_SHOWCOLORHINTS" 'sch' 'bool'
+  __print_variable "GIT_PS1_STATESEPARATOR" "$GIT_PS1_STATESEPARATOR" 'ss' 'str'
+  __print_variable "GIT_PS1_SHOWDIRTYSTATE" "$GIT_PS1_SHOWDIRTYSTATE" 'sds' 'bool'
   echo "  $(__ps1_ansi red)*$(__ps1_ansi reset) unstaged"
   echo "  $(__ps1_ansi green)+$(__ps1_ansi reset) staged  "
-  print_variable 'sss' "GIT_PS1_SHOWSTASHSTATE" $GIT_PS1_SHOWSTASHSTATE
+  __print_variable "GIT_PS1_SHOWSTASHSTATE" "$GIT_PS1_SHOWSTASHSTATE" 'sss' 'bool'
   echo "  $(__ps1_ansi blue)\$$(__ps1_ansi reset) stashed"
-  print_variable 'suf' "GIT_PS1_SHOWUNTRACKEDFILES" $GIT_PS1_SHOWUNTRACKEDFILES
+  __print_variable "GIT_PS1_SHOWUNTRACKEDFILES" "$GIT_PS1_SHOWUNTRACKEDFILES" 'suf' 'bool'
   echo "  $(__ps1_ansi red)%$(__ps1_ansi reset) untracked files"
-  print_variable 'su' "GIT_PS1_SHOWUPSTREAM" $GIT_PS1_SHOWUPSTREAM
+  __print_variable "GIT_PS1_SHOWUPSTREAM" "$GIT_PS1_SHOWUPSTREAM" 'su' 'list'
   echo "  < you're behind"
   echo "  > you're ahead"
   echo "  <> you're diverge"
   echo "  = no difference"
   echo "  $(__ps1_ansi orange)values$(__ps1_ansi white): [verbose][,name][,legacy][,git][,svn]"
-  print_variable 'cps' "GIT_PS1_COMPRESSSPARSESTATE" $GIT_PS1_COMPRESSSPARSESTATE
-  print_variable 'scs' "GIT_PS1_SHOWCONFLICTSTATE" $GIT_PS1_SHOWCONFLICTSTATE
-  print_variable 'pds' "GIT_PS1_DESCRIBE_STYLE" $GIT_PS1_DESCRIBE_STYLE
+  __print_variable "GIT_PS1_COMPRESSSPARSESTATE" "$GIT_PS1_COMPRESSSPARSESTATE" 'cps' 'bool'
+  __print_variable "GIT_PS1_SHOWCONFLICTSTATE" "$GIT_PS1_SHOWCONFLICTSTATE" 'scs' 'bool'
+  __print_variable "GIT_PS1_DESCRIBE_STYLE" "$GIT_PS1_DESCRIBE_STYLE" 'pds' 'selection'
   echo "  $(__ps1_ansi orange)values$(__ps1_ansi white): (contains|branch|describe|tag|default)"
-  print_variable 'dg' "PS1_DISABLE_GIT" $PS1_DISABLE_GIT
-  print_variable 'dn' "PS1_DIR_NOPARSE" $PS1_DIR_NOPARSE
-  print_variable 'ds' "PS1_DIR_SHORTEN" $PS1_DIR_SHORTEN
-  print_variable 'rev' "PS1_REVERSE" $PS1_REVERSE
+  __print_variable "PS1_DISABLE_GIT" "$PS1_DISABLE_GIT" 'dg' 'bool'
+  __print_variable "PS1_DIR_PARSE" "$PS1_DIR_PARSE" 'dn' 'bool'
+  __print_variable "PS1_DIR_SHORTEN" "$PS1_DIR_SHORTEN" 'ds' 'bool'
+  echo "- set to false"
+  echo "+ set to true"
+  echo "= set a value"
 }
 
 function ps1() {
@@ -71,7 +100,6 @@ function ps1() {
           local value=$2
           shift
         fi
-        echo $value
 
         case $flag in
           (sds) GIT_PS1_SHOWDIRTYSTATE=$value;;
@@ -82,9 +110,11 @@ function ps1() {
           (scs) GIT_PS1_SHOWCONFLICTSTATE=$value;;
           (pds) GIT_PS1_DESCRIBE_STYLE=$value;;
           (dg) PS1_DISABLE_GIT=$value;;
-          (dn) PS1_DIR_NOPARSE=$value;;
+          (dp) PS1_DIR_PARSE=$value;;
           (ds) PS1_DIR_SHORTEN=$value;;
-          (rev) PS1_REVERSE=$value;;
+          (*)
+            echo "unknown '$flag' flag"
+            return 1
         esac;;
       (*)
         echo "'$1' flag must starts with + or - or ="
@@ -94,25 +124,45 @@ function ps1() {
   done
 }
 
+function __ps1_unansi() {
+  echo $1 | sed "s/$(printf '\001\e[[0-9]*m\002')//g"
+}
+
 function __ps1() {
   local RETVAL=$?
-  local git_state=$([[ -n $PS1_DISABLE_GIT ]] && echo "" || __git_ps1 "(%s)")
-  local dir=${PWD#$HOME}
-  local status=""
 
+  # git: Git {
+  local git_state=$([[ -n $PS1_DISABLE_GIT ]] && echo "" || __git_ps1 "(%s)")
+  # }
+
+  # stat: Status {
+  local status
   [[ $RETVAL -ne 0 ]] && status+="$(__ps1_ansi red)$RETVAL"
   [[ $UID -eq 0 ]] && status+="$(__ps1_ansi yellow)#"
   [[ $(jobs -l | wc -l) -gt 0 ]] && status+="$(__ps1_ansi cyan)~"
-  [[ -n $status ]] && status="[$status$(__ps1_ansi reset)]" || status=""
+  [[ -n $status ]] && status="[$status$(__ps1_ansi reset)]"
+  # }
 
-  local status_git="$git_state$status"
+  # gas: Git state And Status (git|stat) {
+  local gas="$git_state$status"
+  local gas_raw=$(__ps1_unansi $gas)
+  local gas_len=${#gas_raw}
+  # }
 
+  # sep: Separator (gas) {
+  local separator
+  local separator_len
+  [[ -z $gas_raw ]] && separator+='' || separator+=' 󰁔'
+  separator+=' '
+  separator_len=${#separator}
+  separator="$(__ps1_ansi blue)$separator$(__ps1_ansi reset)"
+  # }
 
+  # dir: Directory (sep|raw) {
+  local dir=${PWD#$HOME}
   [[ $dir != $PWD ]] && dir="~$dir"
-  if [[ -z $PS1_DIR_NOPARSE ]]; then
-    local raw
-    [[ -n $status_git ]] && raw=$(printf $status_git | sed "s/$(printf '\001\e[[0-9]*m\002')//g")
-    if [[ ${#dir} -gt $(expr $COLUMNS - 3 - ${#raw}) ]]; then
+  if [[ -n $PS1_DIR_PARSE ]]; then
+    if [[ ${#dir} -gt $(expr $COLUMNS - $separator_len - ${#gas_raw}) ]]; then
       local base_name=$(basename $dir)
       local dir_name=$(dirname $dir)
       dir=""
@@ -124,22 +174,7 @@ function __ps1() {
       dir+="$base_name"
     fi
   fi
-  
-  local left="$status_git"
-  local right="$dir"
-
-  local arrow="$(__ps1_ansi blue)"
-  if [[ -n $PS1_REVERSE ]]; then
-    localdion temp=$left
-    left=$right
-    right=$temp
-    arrow+="󰁍"
-  else
-    arrow+="󰁔"
-  fi
-  arrow+="$(__ps1_ansi reset)"
-
-  [[ -n $left ]] && left+=" $arrow "
-  printf '%s%s\n$ ' "$left" "$right"
+  # }
+  printf "$gas$separator$dir\n\$ "
 }
 PS1='$(__ps1)'
