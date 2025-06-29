25700da88e2f564e035b9d31ee4fadd3303dde76
NexushasTaken <nexushastaken@gmail.com>
2025-06-30 03:39:15 +0800

    change Accel Speed

--- mouserc.sh
+++ mouserc.sh
@@ -1,4 +1,4 @@
 mouse_device=$(cat ~/.config/mouse-id.txt 2> /dev/null)
 device=${mouse_device:-'USB OPTICAL MOUSE'}
-xinput --set-prop "$device" 'libinput Accel Speed' -0.6
+xinput --set-prop "$device" 'libinput Accel Speed' -0.8
 xinput --set-prop "$device" 'libinput Accel Profile Enabled' 0 0
