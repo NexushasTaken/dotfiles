d6c160c9734e61d786064e3250c32ed71f3c4b6a
NexushasTaken <nexushastaken@gmail.com>
2025-07-09 14:11:52 +0800

    add and modify configs
    add krita configs
    add kitty config
    modify i3status config
    disable picom autostart in i3wm config
    add --tabs to `bat` config

--- mouserc.sh
+++ mouserc.sh
@@ -1,4 +1,4 @@
-mouse_device=$(cat ~/.config/mouse-id.txt 2> /dev/null)
+use_device=$(cat ~/.config/mouse-id.txt 2> /dev/null)
 device=${mouse_device:-'USB OPTICAL MOUSE'}
-xinput --set-prop "$device" 'libinput Accel Speed' -0.8
+xinput --set-prop "$device" 'libinput Accel Speed' -1.0
 xinput --set-prop "$device" 'libinput Accel Profile Enabled' 0 0
